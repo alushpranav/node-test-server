e578ff4058220ac8af670282ae9856442b3cf574
kishornlstech <kg@nlstech.net>
2023-05-07 23:19:42 +0530

    port 3k

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.get('/', (req, res) => {
     res.json({ msg: "Server Running...!" })
 })
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.log('Server listening on ' + port)
